0fc450bd9f4d85dd3d1efd871932127210b82b0b
Alexey2607 <120499593+Alexey2607@users.noreply.github.com>
2022-12-29 12:40:36 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+// Console.WriteLine("Введите сколько чисел Вы будете вводить");
+// int N = Convert.ToInt32(Console.ReadLine());
+
+// int I = 1;
+// int count = 0;
+
+// while (I <= N)
+// {
+//     Console.WriteLine("Введите число");
+//     int num = Convert.ToInt32(Console.ReadLine());
+
+//     I++;
+
+//     if (num > 0)
+//     { count++; }
+
+// }
+// Console.WriteLine("Количество положительных чисел   " + count);
+
+// ______________________________________________________________________
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух 
+// прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// Console.WriteLine("Введите значение k1");
+// double k1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите значение b1");
+// double b1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите значение k2");
+// double k2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите значение b2");
+// double b2 = Convert.ToInt32(Console.ReadLine());
+
+// if (k1 == k2)
+// {
+//     Console.WriteLine("Прямые не пересекаются");
+// }
+// else
+// {
+//     double x = (b2-b1)/(k1-k2);
+//     double y = k1*x+b1;
+//     Console.WriteLine("Точка пересечения:" + "( " + x + ";" + y + " )");
+// }
+// __________________________________________________________________________________
